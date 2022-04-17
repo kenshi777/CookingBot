70d9b9f160eadb7025580bcd5409ed8bfa98af2e
kenshi777 <bolev599@gmail.com>
2022-04-17 19:41:12 +0300

    Base of the program

---
+++ main.py
@@ -0,0 +1,39 @@
+from unicodedata import name
+import requests
+from pprint import pprint
+
+def get_meal(meal):
+    try:
+        r = requests.get(
+            f"http://www.themealdb.com/api/json/v1/1/search.php?s={meal}"
+        ).json()
+        #pprint(r)
+        name = r['meals'][0]['strMeal']
+        country = r['meals'][0]['strArea']
+        category = r['meals'][0]['strCategory']
+        ingredients_list = []
+        for i in range(1, 20):
+            if (r['meals'][0][f'strIngredient{i}'] != '') and (r['meals'][0][f'strIngredient{i}'] != None) and (r['meals'][0][f'strMeasure{i}'] != ' '):
+                ingredients_list.append(r['meals'][0][f'strIngredient{i}'])
+        instruction = r['meals'][0]['strInstructions']
+        image = r['meals'][0]['strMealThumb']
+        measures_list = []
+        for m in range(1, 20):
+            if (r['meals'][0][f'strMeasure{m}'] != '') and (r['meals'][0][f'strMeasure{m}'] != None) and (r['meals'][0][f'strMeasure{m}'] != ' '):
+                measures_list.append(r['meals'][0][f'strMeasure{m}'])
+        tags = r['meals'][0]['strTags']
+        video = r['meals'][0]['strYoutube']
+        print(
+            f"Meal: {name}\nCountry: {country}\nCategory: {category}\nIngredients: {ingredients_list}\nMesaures: {measures_list}\nInstruction: {instruction}\nTags: {tags}\nYoutube: {video}\nEnjoy your meal!"
+        )
+    except Exception as ex:
+        print(ex)
+        print("Unknown meal!")
+
+def main():
+    meal = input("Enter meal: ")
+    get_meal(meal)
+
+
+if __name__ == '__main__':
+    main()
