f5829923cf169a35003e65a5a07047c218ba4b69
kenshi777 <bolev599@gmail.com>
2022-04-20 23:26:43 +0300

    Telegram

--- bot.py
+++ bot.py
@@ -1,9 +1,18 @@
-from email import message
-from unicodedata import name
 import requests
-from pprint import pprint
+from config import TOKEN
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
-def get_meal(meal):
+bot = Bot(token=TOKEN)
+disp = Dispatcher(bot)
+
+@disp.message_handler(commands=["start"])
+async def start_command(message: types.message):
+    await message.reply("Hello!")
+
+@disp.message_handler(commands=[])
+async def get_meal(message: types.message):
     try:
         r = requests.get(
             f"http://www.themealdb.com/api/json/v1/1/search.php?s={message.text}"
@@ -23,16 +32,11 @@ def get_meal(meal):
                 measures_list.append(r['meals'][0][f'strMeasure{m}'])
         tags = r['meals'][0]['strTags']
         video = r['meals'][0]['strYoutube']
-        print(
+        await message.reply(
             f"Meal: {name}\nCountry: {country}\nCategory: {category}\nIngredients: {ingredients_list}\nMesaures: {measures_list}\nInstruction: {instruction}\nTags: {tags}\nYoutube: {video}\nEnjoy your meal!"
         )
     except:
-        print("Unknown meal!")
+        await message.reply("Unknown meal!")
 
-def main():
-    meal = input("Enter meal: ")
-    get_meal(meal)
-
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    executor.start_polling(disp)
