ffbf9e011d4e4de3110723a34e81bf7c14b031b8
kenshi777 <bolev599@gmail.com>
2022-04-25 23:30:24 +0300

    Добавил кнопки

--- bot.py
+++ bot.py
@@ -1,17 +1,33 @@
 import requests
 from config import TOKEN
 from aiogram import Bot, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
 bot = Bot(token=TOKEN)
 disp = Dispatcher(bot)
 
-@disp.message_handler(commands=["start"])
-async def start_command(message: types.message):
-    await message.reply("Hello!")
+button_1 = KeyboardButton('/Lets_go!')
+button_2 = KeyboardButton('/help')
+button_3 = KeyboardButton('/breakfast')
+button_4 = KeyboardButton('/lunch')
+button_5 = KeyboardButton('/dinner')
 
-@disp.message_handler(commands=[])
+keyboard_1 = ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard_1.add(button_1).add(button_2)
+
+keyboard_2 = ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard_2.row(button_3, button_4, button_5)
+
+@disp.message_handler(commands=['start','help'])
+async def start_command(message: types.message):
+    await message.reply("Hi! I'm a cooking bot! I'll help you choose a dish. Just follow the instructions I'm writing you. Bon Appetit!", reply_markup=keyboard_1)
+@disp.message_handler(commands=['Lets_go!'])
+async def first_step(message: types.message):
+    await message.reply("Good! What do you want to cook?", reply_markup=keyboard_2)
+
+@disp.message_handler()
 async def get_meal(message: types.message):
     try:
         r = requests.get(
